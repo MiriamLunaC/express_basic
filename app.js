f9bcac46573ecb291a86b8559bacf70e5e6d8517
Miriam <miriamlunacarreon133@gmail.com>
2022-05-09 11:23:39 -0500

    Agregando la ruta /explorersInNode

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { process_params } = require('express/lib/router')
 const app = express()
 const port = 3000
 
@@ -8,6 +9,11 @@ app.get('/',(req, res)=>{
 
 app.get('/launchx', (req, res)=>{
     res.send('Bienvenidos a launchx')
+})
+
+app.get('/explorersInNode', (req, res)=>{
+    const explorer = {name: "Explorer", msg:"Hello"}
+    res.send(explorer)
 })
 
 app.listen(port, ()=>{
